d9b7fadd4681fcd56b90407bff527a506e22ada2
Septia Dewi Rahmania <septiadewir.netengineer@gmail.com>
2023-07-06 10:23:35 +0700

    done

--- js/script.js
+++ js/script.js
@@ -7,9 +7,32 @@ function validateForm() {
 
     if (name =="" || email =="" || phone =="" || select =="" || message ==""){
         alert("DATA TIDAK BOLEH KOSONG");
-        return false;
+    }
+    else {
+        alert("DATA BERHASIL DIKIRIM");
     };
     
     console.log({name, email, phone, select, message});
     return false;
 };
+
+function plusDiv(index) {
+    showImage(slideIndex += index)
+}
+
+function showImage(index) {
+    console.log(index)
+    const imgList = document.getElementsByClassName("slider")
+    if (index > imgList.length) {slideIndex = 1}
+    if (index < 1) {slideIndex = imgList.length}
+
+    for(i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = "none"
+    }
+    imgList[slideIndex - 1].style.display = "block"
+}
+
+var slideIndex = 1
+setInterval(function(){
+    plusDiv(1)
+}, 2000)
